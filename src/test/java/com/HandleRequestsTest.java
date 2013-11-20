3e1847ed1a841cf08a8fc2bf79c4cc551519c3c9
bartamihai <mihai.barta@evozon.com>
2013-11-20 13:13:09 +0200

    handleRequestTest

--- src/test/java/com/HandleRequestsTest.java
+++ src/test/java/com/HandleRequestsTest.java
@@ -9,9 +9,7 @@ import net.thucydides.core.annotations.ManagedPages;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Story;
 import net.thucydides.core.pages.Pages;
-import net.thucydides.junit.annotations.Qualifier;
-import net.thucydides.junit.annotations.UseTestDataFrom;
-import net.thucydides.junit.runners.ThucydidesParameterizedRunner;
+import net.thucydides.junit.runners.ThucydidesRunner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,8 +21,8 @@ import com.steps.EndUserSteps;
 import com.steps.UserStepsForVacation;
 
 @Story(Application.HandleRequests.HandleRequestsTest.class)
-@RunWith(ThucydidesParameterizedRunner.class)
-@UseTestDataFrom("resources/DataHandleRequest.csv")
+@RunWith(ThucydidesRunner.class)
+
 public class HandleRequestsTest {
 
 	@Managed(uniqueSession = true)
@@ -33,53 +31,31 @@ public class HandleRequestsTest {
 	@ManagedPages(defaultUrl = "http://172.22.8.38:9090")
 	public Pages pages;
 
-	String user, pass;
-
-	@Qualifier
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	public String getPass() {
-		return pass;
-	}
-
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-
 	@Steps
 	public EndUserSteps endUser;
 
 	@Steps
 	public UserStepsForVacation userStepsForVacation;
-	
+
 	@Steps
 	public CopyOfFolderFetchIMAPStep copyOfFolderFetchIMAPStep;
-	
 
 	@Test
-	public void test_csv_login() throws MessagingException,
-	IOException {
+	public void test_csv_login() throws MessagingException, IOException {
 		endUser.is_the_home_page();
-		endUser.enter_user(getUser());
-		endUser.enter_password(getPass());
+		endUser.enter_user("QaDepartment123@gmail.com");
+		endUser.enter_password("1234");
 		endUser.submit();
 		endUser.should_be_on_department_page();
 		endUser.should_be_on_department_page1();
 		endUser.click_v();
 		userStepsForVacation.clickOnActionOptionForSpecifiedVacation(
-				"Assigned to me", "Approve","");
+				"Assigned to me", "Approve", "test1");
 		endUser.waitABit(5000);
 		userStepsForVacation.clickConfirmingOK();
 		userStepsForVacation.shouldShowConfirmingMessage();
-		copyOfFolderFetchIMAPStep.CheckIfEmailContainsTerms("QaDepartment123", "1234567Qa", false, false, false, false, true, "dear","evoportal team");
+		copyOfFolderFetchIMAPStep.CheckIfEmailContainsTerms("QaDepartment123",
+				"1234567Qa", false, false, false, false, true,"evoportal team");
 	}
-	
-}
-	
 
+}
